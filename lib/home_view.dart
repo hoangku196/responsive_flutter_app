5891b3a418c02323cb1ed9493d0e3fd20a6ad893
Hoang <hoanglxph09371@fpt.edu.vn>
2021-02-03 16:46:42 +0700

    first commit

---
+++ lib/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app/base_widget.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget(
+      builder: (context, sizingInformation) {
+        return Scaffold(
+          body: Center(
+            child: Text(sizingInformation.toString()),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_app/home_view.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
